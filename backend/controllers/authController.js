90da89adada289750a4174747b94d3442a221247
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-03-02 07:50:39 +0100

    send password recovery email

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,61 +1,106 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
-const handleError = require('../utils/handleError');
-const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
-const sendToken = require('../utils/jwtToken');
+const handleError = require("../utils/handleError");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 // Register a user => /api/v1/register
-exports.registerUser = catchAsyncErrors( async (req, res, next) => {
-    const { name, email, password } = req.body;
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    const user = await User.create({
-        name,
-        email,
-        password,
-        avatar: {
-            public_id: 'avatars/photo-1535713875002-d1d0cf377fde_zszh3n.jpg',
-            url: 'https://res.cloudinary.com/dindustack/image/upload/v1614419695/avatars/photo-1535713875002-d1d0cf377fde_zszh3n.jpg'
-        }
-    })
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "avatars/photo-1535713875002-d1d0cf377fde_zszh3n.jpg",
+      url:
+        "https://res.cloudinary.com/dindustack/image/upload/v1614419695/avatars/photo-1535713875002-d1d0cf377fde_zszh3n.jpg",
+    },
+  });
 
-   sendToken(user, 200, res)
-})
+  sendToken(user, 200, res);
+});
 
 // Login user => /a[i/v1/login]
-exports.loginUser = catchAsyncErrors( async(req, res, next) => {
-    const { email, password } = req.body;
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    // Checks if email and password is entered by user
-    if(!email || !password) {
-        return next(new handleError('Please enter email & password', 400))
-    }
+  // Checks if email and password is entered by user
+  if (!email || !password) {
+    return next(new handleError("Please enter email & password", 400));
+  }
 
-    // Finding user in database
-    const user = await User.findOne({ email }).select('+password')
+  // Finding user in database
+  const user = await User.findOne({ email }).select("+password");
 
-    if(!user) {
-        return next(new handleError('Invalid Email or Password', 401));
-    }
+  if (!user) {
+    return next(new handleError("Invalid Email or Password", 401));
+  }
 
-    // Check if password is correct or not
-    const isPasswordMatched = await user.comparePassword(password);
-   
-    if(!isPasswordMatched) {
-        return next(new handleError('Invalid Email or Password', 401));
-    }
+  // Check if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
 
-    sendToken(user, 200, res)
-})
+  if (!isPasswordMatched) {
+    return next(new handleError("Invalid Email or Password", 401));
+  }
 
-// Logout user => /api/v1/logout
-exports.logout = catchAsyncErrors( async (req, res, next) => {
-    res.cookie('token', null, {
-        expires: new Date(Date.now()),
-        httpOnly: true
+  sendToken(user, 200, res);
+});
+
+// forgot password => /api/v1/password/forgot
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new handleError("User not found with this email", 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // Create reset password url
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you haven't requested this email, then ignore it.`;
+
+  try {
+
+    await sendEmail({
+        email: user.email,
+        subject: 'Orange Password Reset',
+        message
     })
 
     res.status(200).json({
         success: true,
-        message: 'Logged out'
+        message: `Email successfully sent to: ${user.email}`
     })
-})
+
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new handleError(error.message, 500))
+  }
+});
+
+// Logout user => /api/v1/logout
+exports.logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged out",
+  });
+});
--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -26,7 +26,7 @@ module.exports = (err, req, res, next) => {
         // Handling Mongoose validation error
         if (err.name === 'ValidationError') {
             const message = Object.values(err.errors).map(value => value.message);
-            error = new ErrorHandler(message, 400)
+            error = new handleError(message, 400)
         }
 
 
