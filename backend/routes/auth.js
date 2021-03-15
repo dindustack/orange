2650f98890ddb3ea864013f3e0180b0e558874b8
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-03-15 08:55:48 +0100

    show user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser, forgotPassword, resetPassword, logout } = require('../controllers/authController');
+const {
+  registerUser,
+  loginUser,
+  forgotPassword,
+  resetPassword,
+  getUserProfile,
+  logout,
+} = require("../controllers/authController");
 
-router.route('/register').post(registerUser);
-router.route('/login').post(loginUser);
+const { isAuthenticatedUser } = require("../middlewares/auth")
 
-router.route('/password/forgot').post(forgotPassword);
-router.route('/password/reset/:token').put(resetPassword)
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
 
-router.route('/logout').get(logout);
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+
+router.route("/me").get(isAuthenticatedUser, getUserProfile);
+
+router.route("/logout").get(logout);
 
 module.exports = router;
