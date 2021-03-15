caf677bb4ed44f6ff99cd5d79e76e6228a4c66d6
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-03-15 14:08:31 +0100

    update user profile

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,8 +7,9 @@ const {
   forgotPassword,
   resetPassword,
   getUserProfile,
-  logout,
   updatePassword,
+  updateProfile,
+  logout,
 } = require("../controllers/authController");
 
 const { isAuthenticatedUser } = require("../middlewares/auth")
@@ -21,6 +22,7 @@ router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
 router.route("/password/update").put(isAuthenticatedUser,updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
 router.route("/logout").get(logout);
 
