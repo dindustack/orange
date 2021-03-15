69fe0e0900382cb785d4f53e129cf8745a5c7a78
Chinwendu Agbaetuo <silviachinwendu@gmail.com>
2021-03-15 22:48:58 +0100

    admin -update and delete user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -149,7 +149,7 @@ exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
   sendToken(user, 200, res)
 })
 
-// Update user profile 
+// Update user profile  +> /api/v1/me/update
 exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
   const newUserData = {
     name: req.body.name,
@@ -203,5 +203,39 @@ exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     user
+  })
+})
+
+// Update user profile  => /api/v1/admin/user/:id
+exports.updateUser = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role
+  }
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false
+  })
+
+  res.status(200).json({
+    success: true
+  })
+})
+
+// Delete user => /api/v1/admin/user/:id
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if(!user) {
+    return next(new handleError(`User does not found with id: ${req.params.id}`))
+  }
+
+  await user.remove();
+
+  res.status(200).json({
+    success: true,
   })
 })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,9 +12,11 @@ const {
   logout,
   allUsers,
   getUserDetails,
+  updateUser,
+  deleteUser,
 } = require("../controllers/authController");
 
-const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth")
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
@@ -23,14 +25,18 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
-router.route("/password/update").put(isAuthenticatedUser,updatePassword);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
-router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers)
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allUsers);
 
 router
   .route("/admin/user/:id")
-  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails);
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getUserDetails)
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUser)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 
 router.route("/logout").get(logout);
 
